1a574987514d8d9b793cd6d7274d63f4b8a23943
Vasek - Tom C <tom@quartz.technology>
2023-10-03 14:32:21 +0100

    feat: add publish command
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

--- node/commands.go
+++ node/commands.go
@@ -1,6 +1,10 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
+)
 
 // Run returns the container with the command set.
 func (n *Node) Run(args []string) *Container {
@@ -9,27 +13,52 @@ func (n *Node) Run(args []string) *Container {
 
 // Start returns the output of the code executed.
 func (n *Node) Start(ctx context.Context) (string, error) {
-	return n.Run([]string{"start"}).Stdout(ctx)
+	return n.Run([]string{"run", "start"}).Stdout(ctx)
 }
 
 // Lint returns the output of the lint command.
 func (n *Node) Lint(ctx context.Context) (string, error) {
-	return n.Run([]string{"lint"}).Stdout(ctx)
+	return n.Run([]string{"run", "lint"}).Stdout(ctx)
 }
 
 // Test returns the result of the test executed.
 func (n *Node) Test(ctx context.Context) (string, error) {
-	return n.Run([]string{"test"}).Stdout(ctx)
+	return n.Run([]string{"run", "test"}).Stdout(ctx)
 }
 
-// Build returns the directory of the source built.
-func (n *Node) Build(ctx context.Context) (*Directory, error) {
-	result, err := n.Run([]string{"build"}).Sync(ctx)
+// Build returns the Container with the source built.
+func (n *Node) Build() *Node {
+	n.Ctr = n.Run([]string{"run", "build"})
+
+	return n
+}
+
+type PublishOpts struct {
+	Token   *Secret `doc:"Secret token to register to npm registry"`
+	Version string  `doc:"version of the package"`
+	Access  string  `doc:"access permission of the package"`
+}
+
+// Publish publishes the source code to npm registry.
+func (n *Node) Publish(ctx context.Context, opts PublishOpts) (string, error) {
+	token, err := opts.Token.Plaintext(ctx)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return result.Directory("dist"), nil
+	// Configure .npmrc
+	npmrc := fmt.Sprintf(`//registry.npmjs.org/:_authToken=%s
+registry=https://registry.npmjs.org/
+always-auth=true`, token)
+	if err := os.WriteFile(".npmrc", []byte(npmrc), 0o600); err != nil {
+		return "", err
+	}
+
+	// Set version and publish
+	return n.Ctr.
+		WithExec([]string{"version", opts.Version}).
+		WithExec([]string{"publish", "--access", opts.Access}).
+		Stdout(ctx)
 }
 
 type InstallOpts struct {
--- node/npm.go
+++ node/npm.go
@@ -3,7 +3,7 @@ package main
 // WithNpm returns Node container with npm configured as package manager.
 func (n *Node) WithNpm() *Node {
 	n.Ctr = n.Ctr.
-		WithEntrypoint([]string{"npm", "run"}).
+		WithEntrypoint([]string{"npm"}).
 		WithMountedCache("/root/.npm", dag.CacheVolume("npm-cache"))
 
 	return n
